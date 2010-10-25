4f06a8f22aa789aa5572acb6bfdcc8ee5320d9a9
Miquel Torres <tobami@googlemail.com>
2010-10-25 13:24:06 +0200

    Move user and password info to an external file Check that recipe and role exist Add debug loglevel option

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,32 @@
 # -*- coding: utf-8 -*-
-# LittleChef: Configuration management
-# using Chef Solo in a push based system using Python and Fabric
+# LittleChef: Configuration management using Chef Solo
+# in a push based system using Python and Fabric
 import fabric
 from fabric.api import *
 from fabric.contrib.files import upload_template, append
 import ConfigParser, os
 import simplejson as json
 
-env.user     = ""
-env.password = ""
-NODEPATH     = "nodes/"
-APPNAME      = 'littlechef'
+
+NODEPATH = "nodes/"
+APPNAME  = "littlechef"
+
+def _readconfig():
+    '''Read main fabric configuration'''
+    config = ConfigParser.ConfigParser()
+    config.read("auth.cfg")
+    try:
+        try:
+            env.user = config.get('userinfo', 'user')
+            if not env.user: raise ValueError
+        except (ConfigParser.NoOptionError, ValueError):
+            abort('You need to define a valid user in auth.cfg')
+        env.password = config.get('userinfo', 'password')
+    except ConfigParser.NoSectionError:
+        abort('No section "userinfo" found in auth.cfg. Refer to the README for help')
+    env.loglevel = "info"
+
+_readconfig()
 
 def _get_nodes():
     nodes = []
@@ -30,11 +46,19 @@ def node(host):
     '''Select a node'''
     env.hosts = [host]
 
+@hosts('setup')
+def debug():
+    '''Sets logging level to debug'''
+    print "Setting logging level to 'debug'..."
+    env.loglevel = 'debug'
+
 def recipe(recipe, save=False):
     '''Execute the given recipe,ignores existing config'''
     with hide('stdout', 'running'): hostname = run('hostname')
     print "\n== Executing recipe %s on node %s ==" % (recipe, hostname)
     configfile = hostname + ".json"
+    if not os.path.exists('cookbooks/' + recipe):
+        abort("Recipe '%s' not found" % recipe)
     data = {
         APPNAME: {'nodename': hostname, 'nodeid': env.host_string},
         "run_list": [ "recipe[%s]" % recipe ],
@@ -46,6 +70,8 @@ def role(role, save=False):
     '''Execute the given role, ignores existing config'''
     with hide('stdout', 'running'): hostname = run('hostname')
     print "\n== Applying role %s to node %s ==" % (role, hostname)
+    if not os.path.exists('roles/' + role):
+        abort("Role '%s' not found" % role)
     data = {
         APPNAME: {'nodename': hostname, 'nodeid': env.host_string},
         "run_list": [ "role[%s]" % role ],
@@ -147,7 +173,7 @@ def _configure_node(configfile):
             use_sudo=True
         )
         print "Cooking..."
-        sudo('chef-solo -j /etc/chef/node.json')
+        sudo('chef-solo -l %s -j /etc/chef/node.json' % env.loglevel)
 
 def _update_cookbooks():
     print "Uploading cookbooks..."
