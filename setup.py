1db7b57a20b9b72261a5cf3fcea23d58164a12ae
Miquel Torres <tobami@googlemail.com>
2010-11-29 11:22:30 +0100

    Tag 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 setup(
     name = "littlechef",
-    version = "0.3.2",
+    version = "0.3.3",
     description = "Cook with Chef without a Chef Server",
     author = "Miquel Torres",
     author_email = "tobami@googlemail.com",
