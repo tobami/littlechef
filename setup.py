2976bb81b8afbc94534215f675c13b7bffbc8b34
Nicolas Pinto <pinto@mit.edu>
2011-04-24 10:39:29 -0400

    MISC: setup.py now pep8 safe

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 # LittleChef's setup.py
 from distutils.core import setup
 setup(
-    name = "littlechef",
-    version = __import__('littlechef').version,
-    description = "Cook with Chef without a Chef Server",
-    author = "Miquel Torres",
-    author_email = "tobami@googlemail.com",
-    url = "http://github.com/tobami/littlechef",
-    download_url = "http://github.com/tobami/littlechef/archives/master",
-    keywords = ["chef", "devops"],
+    name="littlechef",
+    version=__import__('littlechef').version,
+    description="Cook with Chef without a Chef Server",
+    author="Miquel Torres",
+    author_email="tobami@googlemail.com",
+    url="http://github.com/tobami/littlechef",
+    download_url="http://github.com/tobami/littlechef/archives/master",
+    keywords=["chef", "devops"],
     install_requires=['fabric>=1.0.1', 'simplejson'],
-    py_modules = ['littlechef/littlechef', 'littlechef/version'],
-    scripts = ['cook'],
+    py_modules=['littlechef/littlechef', 'littlechef/version'],
+    scripts=['cook'],
     zip_safe=False,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Environment :: Console",
@@ -22,14 +22,23 @@ setup(
         "Operating System :: OS Independent",
         'Topic :: System :: Systems Administration',
         ],
-    long_description = """\
+    long_description="""\
 Cook with Chef without Chef Server
 -------------------------------------
 With LittleChef you will get all you need to start cooking with Chef_.
 
-It works as follows: Whenever you apply a recipe to a node, all needed cookbooks and its dependencies are gzipped and uploaded to that node. A node.json file gets created on the fly and uploaded, and Chef Solo gets executed at the remote node, using node.json as the node configuration and the pre-installed solo.rb for Chef Solo configuration. Cookbooks and roles are configured to be found at (/var/chef-solo/).
+It works as follows: Whenever you apply a recipe to a node, all needed
+cookbooks and its dependencies are gzipped and uploaded to that node. A
+node.json file gets created on the fly and uploaded, and Chef Solo gets
+executed at the remote node, using node.json as the node configuration and the
+pre-installed solo.rb for Chef Solo configuration. Cookbooks and roles are
+configured to be found at (/var/chef-solo/).
 
-The result is that you can play as often with your recipes and nodes as you want, without having to worry about a central Chef repository, Chef server nor anything else. You can make small changes to your cookbooks and test them again and again without having to commit the changes. LittleChef brings back sanity to cookbook development.
+The result is that you can play as often with your recipes and nodes as you
+want, without having to worry about a central Chef repository, Chef server nor
+anything else. You can make small changes to your cookbooks and test them again
+and again without having to commit the changes. LittleChef brings back sanity
+to cookbook development.
 
 .. _Chef: http://wiki.opscode.com/display/chef/Home
 """
