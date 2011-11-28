28185f778ec715f75dfb0649228427337cb2ab41
Miquel Torres <tobami@googlemail.com>
2011-11-28 17:58:42 +0100

    Improve error message when failing to generate metadata.json when the local Chef version is 0.8.x

--- littlechef/lib.py
+++ littlechef/lib.py
@@ -96,6 +96,9 @@ def _generate_metadata(path, cookbook_path, name):
         (not os.path.exists(metadata_path_json) or
          os.stat(metadata_path_rb).st_mtime > \
          os.stat(metadata_path_json).st_mtime)):
+        error_msg = "Warning: metadata.json for {0}".format(name)
+        error_msg += " in {0} is older that metadata.rb".format(cookbook_path)
+        error_msg += ", and cookbook attributes could be out of date\n"
         try:
             proc = subprocess.Popen(
                 ['knife', 'cookbook', 'metadata', '-o', cookbook_path, name],
@@ -103,22 +106,25 @@ def _generate_metadata(path, cookbook_path, name):
             resp, error = proc.communicate()
             if ('ERROR:' in resp or 'FATAL:' in resp \
                 or 'Generating metadata for' not in resp):
-                msg = "Unkown error while generating metadata.json for "
-                msg += "{0}. Cookbook attributes may be out of date".format(
-                    path)
-                print(msg)
+                if("No user specified, pass via -u or specifiy 'node_name'"
+                    in error):
+                    error_msg += "You need to have an up-to-date (>=0.10.x)"
+                    error_msg += " version of knife installed locally in order"
+                    error_msg += " to generate metadata.json.\nError "
+                else:
+                    error_msg += "Unkown error "
+                error_msg += "while executing knife to generate "
+                error_msg += "metadata.json for {0}".format(path)
+                print(error_msg)
                 if env.loglevel == 'debug':
                     print "\n".join(resp.split("\n")[:2])
             else:
                 print("Generated metadata.json for {0}".format(path))
         except OSError:
             knife_installed = False
-            msg = "Warning: metadata.json for {0}".format(name)
-            msg += " in {0} is older that metadata.rb".format(cookbook_path)
-            msg += ", and cookbook attributes could be out of date\n"
-            msg += "If you locally install Chef's knife tool, LittleChef will"
-            msg += " regenerate metadata files automatically"
-            print(msg)
+            error_msg += "If you locally install Chef's knife tool, LittleChef"
+            error_msg += " will regenerate metadata.json files automatically"
+            print(error_msg)
 
 
 def get_recipes_in_cookbook(name):
@@ -352,7 +358,7 @@ def credentials(*args, **kwargs):
     """
     if env.ssh_config:
         credentials = env.ssh_config.lookup(env.host)
-        # translate from paramiko params to fabric params
+        # translate from ssh params to fabric params
         if 'identityfile' in credentials:
             credentials['key_filename'] = credentials['identityfile']
         credentials.update(kwargs)
