bf1f2093da03a50e712acb1dd8353d560a0cf2ea
Miquel Torres <tobami@googlemail.com>
2011-12-15 11:49:06 +0100

    Add a plugins directory and the first plugin "save_ip" Removed as a library function. Use now as fix node:xxx plugins:save_ip

---
+++ plugins/save_ip.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+import re
+
+from fabric.api import env
+
+from littlechef import chef
+
+
+def parse_ip(text):
+    """Extract an IPv4 IP from a text string
+    Uses an IP Address Regex: http://www.regular-expressions.info/examples.html
+
+    """
+    ip_matches = re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', text)
+    ip = ip_matches[0] if ip_matches else None
+    return ip
+
+
+def execute(node):
+    proc = subprocess.Popen(['ping', '-c', '1', node['name']],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    resp, error = proc.communicate()
+    if not error:
+        # Split output into lines and parse the first line to get the IP
+        ip = parse_ip(resp.split("\n")[0])
+        if not ip:
+            print "Warning: could not get IP address from node {0}".format(
+                node['name'])
+        print "Node {0} has IP {1}".format(node['name'], ip)
+        # Update with the ipaddress field in the corresponding node.json
+        node['ipaddress'] = ip
+        os.remove(chef.save_config(node, ip))
+    else:
+        print "Warning: could not resolve node {0}".format(node['name'])
