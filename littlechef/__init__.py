154e7b087d73705e82dc2f3263a968dcf0806727
Miquel Torres <tobami@googlemail.com>
2012-01-03 16:33:03 +0100

    Tag 1.1.0

--- littlechef/__init__.py
+++ littlechef/__init__.py
@@ -14,7 +14,7 @@ Server users: data bag search, and node search.
 .. _Chef: http://wiki.opscode.com/display/chef/Home
 
 """
-__version__ = "1.1.0alpha"
+__version__ = "1.1.0"
 __author__ = "Miquel Torres <tobami@gmail.com>"
 
 __cooking__ = False
