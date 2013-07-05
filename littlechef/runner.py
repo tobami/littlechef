505beb82b19ecaeb37625feef084d32c3f7a2ad5
Miquel Torres <tobami@gmail.com>
2013-07-05 16:03:51 +0200

    Don't assume ohai's output always contains 'platform' attributes

--- littlechef/runner.py
+++ littlechef/runner.py
@@ -184,7 +184,8 @@ def deploy_chef(gems="no", ask="yes", version="0.10",
         solo.install(distro_type, distro, gems, version, stop_client)
         solo.configure()
 
-        # Build a basic node file if there isn't one already with some properties from ohai
+        # Build a basic node file if there isn't one already
+        # with some properties from ohai
         with settings(hide('stdout'), warn_only=True):
             output = sudo('ohai -l warn')
         if output.succeeded:
@@ -196,7 +197,7 @@ def deploy_chef(gems="no", ask="yes", version="0.10",
             node = {"run_list": []}
             for prop in ["ipaddress", "platform", "platform_family",
                          "platform_version"]:
-                if ohai[prop]:
+                if ohai.get(prop):
                     node[prop] = ohai[prop]
             chef.save_config(node)
 
