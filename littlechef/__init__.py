7493604d0794ce2a9d5c9c034a31577f3a9b39ed
Issa Ashwash <issa@area1security.com>
2016-01-19 18:23:08 -0800

    Minor version bump

--- littlechef/__init__.py
+++ littlechef/__init__.py
@@ -15,7 +15,7 @@ Server users: data bag search, and node search.
 
 """
 import os
-__version__ = "1.8.0"
+__version__ = "1.8.1"
 __author__ = "Miquel Torres <tobami@gmail.com>"
 
 __cooking__ = False
